920fb81115bf5cd950518c2a7b2b9a2fc4fad7aa
fbc623 <fbc623@gmail.com>
2020-02-02 19:54:15 -0300

    adicionando funcionalidade de disparo de email ao cancelar agendamento

---
+++ API/src/config/mail.js
@@ -0,0 +1,16 @@
+
+export default{
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '4a2544e845d5a2',
+    pass: '3086230b498481',
+   },
+   default:{
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+   }
+};
+
+//mailgun ou sparkpost o mandril ou amazon ses
+//mailtrap para desenvolvimento
